f4edbd3707abd995d4e5e247cb1bb0b0c0baf64a
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-11-17 10:49:41 +0700

    new

---
+++ .history/TypescriptMapExample/file company-info_20221117104923.ts
@@ -0,0 +1,31 @@
+// let map = new Map();
+// map.set('22', 'Infosys');
+// map.set(32, 'TCS');
+// map.set(false,'Google');
+// map.set('45','Facebook');
+// map.set('65','Amazon');
+// console.log("Employer at index 22: " +map.get('22'));
+// console.log("Employer at index 32: " +map.get(32));
+// console.log("Size of the map: " +map.size);
+// console.log("Deleting a value: " +map.delete('65'));
+// console.log( "New Size of the map: " +map.size);
+
+// let employeeMap = new Map();
+// employeeMap.set("Saideep", 20);
+// employeeMap.set("Karthik", 25);
+// employeeMap.set("Sumit", 21);
+// employeeMap.set("Sameer", 22);
+// employeeMap.set("Raje", 24);
+// //Iterating over map keys
+// for (let name of employeeMap.keys()) {
+//     console.log("Employee Names= " +name);
+// }
+// //Iterating over map values
+// for (let age of employeeMap.values()) {
+//     console.log("Employee Age= " +age);
+// }
+// console.log("The employeeMap Entries are: ");
+// //Iterating over map entries
+// for (let entry of employeeMap.entries()) {
+//     console.log(entry[0], entry[1]);
+// }
